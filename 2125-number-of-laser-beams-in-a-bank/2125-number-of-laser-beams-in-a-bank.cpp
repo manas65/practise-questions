6de40f2562a757467b5e38a4eefb19002b596a76
manas65 <65081149+manas65@users.noreply.github.com>
2022-01-08 14:54:39 +0530

    Time: 133 ms (68.66%), Space: 22.9 MB (99.24%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int>lazers;
+       // vector<int>lazers;
+        int prev=0,next=0, ans=0;
         for(int i=0;i<bank.size();i++){
             int count=0;
             for(int j=0;j<bank[0].size();j++){
                 if(bank[i][j]=='1')count++;
             }
-            lazers.push_back(count);
-        }
-    
-        int prev=0,next=0, ans=0;
-        for(int i=0;i<lazers.size();i++){
-            if(lazers[i]==0)continue;
-            else if(prev==0)prev=lazers[i];
-            else if(next==0)next=lazers[i];
+            //space optimized
+            if(prev==0)prev=count;
+            else if(next==0)next=count;
             
             if(prev!=0 && next!=0){
                 ans+=(prev*next);
                 prev=next;
                 next=0;
             }
+            
+            
         }
+    
         return ans;
     }
 };
-//3 0 2 1
-
-
-//0 3 0
-
-//  0 1 0 2 4 0 8 9 9
