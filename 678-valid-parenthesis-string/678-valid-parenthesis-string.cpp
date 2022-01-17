5920b9ede277224ca90100cd1944a6d01bc2a269
manas65 <65081149+manas65@users.noreply.github.com>
2022-01-17 22:34:24 +0530

    Time: 3 ms (23.66%), Space: 6.3 MB (30.34%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-     //   if(s.size()==1)return false;
+    
        stack<int>st1,st2;
         for(int i=0;i<s.length();i++){
             if(s[i]=='*')st2.push(i);
