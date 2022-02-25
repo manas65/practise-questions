d569acd1c4b4f5eb4e3a7f61965205244972b487
manas65 <65081149+manas65@users.noreply.github.com>
2022-02-25 17:05:39 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int helper(int &idx, int& n, string& s){
+         int val=0;
+        if(idx<n && s[idx]=='.'){
+                idx++;
+                while(s[idx]=='0')idx++;//skip leading zero
+              while(idx<n &&s[idx]!='.' ){
+                  val*=10;
+                  val+=s[idx]-'0';
+                  idx++;
+              }  
+                
+            }
+        return val;
+    }
+    
+//     int starter(int &idx, int& n, string& s){
+//         while(s[i]=='0')i++;//skip leading zero
+//               while(i<n && s1[i]!='.' ){
+//                   a*=10;
+//                   a+=(s1[i]-'0');
+                  
+//                   i++;
+//               }  
+//     }
+    
+    
     int compareVersion(string s1, string s2) {
      
         int n=s1.size(),m=s2.size(),i=0,j=0;
@@ -24,31 +50,11 @@ public:
             // cout<<b;
             if(a>b)return 1;
             else if(b>a) return -1;
-        while(i<n || j<m){
-           a=0;
-             b=0;
-            if(i<n && s1[i]=='.'){
-                i++;
-                while(s1[i]=='0')i++;//skip leading zero
-              while(i<n &&s1[i]!='.' ){
-                  a*=10;
-                  a+=s1[i]-'0';
-                  i++;
-              }  
-                
-            }
-            if(j<m && s2[j]=='.'){
-                j++;
-                while(s2[j]=='0')j++;//skip leading zero
-              while(j<m && s2[j]!='.'){
-                  b*=10;
-                  b+=s2[j]-'0';
-                  j++;
-              }  
-                
-            }
-            if(a>b)return 1;
-            else if(b>a) return -1;
+             while(i<n || j<m){
+             a=helper(i,n,s1);
+             b=helper(j,m,s2);
+             if(a>b)return 1;
+             else if(b>a) return -1;
         }
         return 0;
         
