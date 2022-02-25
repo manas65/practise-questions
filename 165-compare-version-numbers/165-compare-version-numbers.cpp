0cd14a713388718c9c96eb37014136d75fd92b2a
manas65 <65081149+manas65@users.noreply.github.com>
2022-02-25 17:07:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.32%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -15,37 +15,40 @@ public:
         return val;
     }
     
-//     int starter(int &idx, int& n, string& s){
-//         while(s[i]=='0')i++;//skip leading zero
+    int starter(int &idx, int& n, string& s){
+        int val=0;
+        while(s[idx]=='0')idx++;//skip leading zero
+              while(idx<n && s[idx]!='.' ){
+                  val*=10;
+                  val+=(s[idx]-'0');
+                  
+                  idx++;
+              }  
+        return val;
+    }
+    
+    
+    int compareVersion(string s1, string s2) {
+     
+        int n=s1.size(),m=s2.size(),i=0,j=0;
+        int a=starter(i,n,s1);
+        int b=starter(j,m,s2);;
+          
+//               while(s1[i]=='0')i++;//skip leading zero
 //               while(i<n && s1[i]!='.' ){
 //                   a*=10;
 //                   a+=(s1[i]-'0');
                   
 //                   i++;
 //               }  
-//     }
-    
-    
-    int compareVersion(string s1, string s2) {
-     
-        int n=s1.size(),m=s2.size(),i=0,j=0;
-        int a=0,b=0;
-          
-              while(s1[i]=='0')i++;//skip leading zero
-              while(i<n && s1[i]!='.' ){
-                  a*=10;
-                  a+=(s1[i]-'0');
-                  
-                  i++;
-              }  
                 // cout<<a<<endl;
           
-                while(s2[j]=='0')j++;//skip leading zero
-              while(j<m && s2[j]!='.' ){
-                  b*=10;
-                  b+=(s2[j]-'0');
-                  j++;
-              }  
+//                 while(s2[j]=='0')j++;//skip leading zero
+//               while(j<m && s2[j]!='.' ){
+//                   b*=10;
+//                   b+=(s2[j]-'0');
+//                   j++;
+//               }  
             
             // cout<<b;
             if(a>b)return 1;
