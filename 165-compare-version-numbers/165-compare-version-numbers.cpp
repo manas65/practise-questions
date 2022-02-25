201bc752287473179543918e989dc3570a7fc318
manas65 <65081149+manas65@users.noreply.github.com>
2022-02-25 17:08:46 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -31,34 +31,17 @@ public:
     int compareVersion(string s1, string s2) {
      
         int n=s1.size(),m=s2.size(),i=0,j=0;
-        int a=starter(i,n,s1);
-        int b=starter(j,m,s2);;
+        int v1=starter(i,n,s1);
+        int v2=starter(j,m,s2);;
           
-//               while(s1[i]=='0')i++;//skip leading zero
-//               while(i<n && s1[i]!='.' ){
-//                   a*=10;
-//                   a+=(s1[i]-'0');
-                  
-//                   i++;
-//               }  
-                // cout<<a<<endl;
-          
-//                 while(s2[j]=='0')j++;//skip leading zero
-//               while(j<m && s2[j]!='.' ){
-//                   b*=10;
-//                   b+=(s2[j]-'0');
-//                   j++;
-//               }  
-            
-            // cout<<b;
-            if(a>b)return 1;
-            else if(b>a) return -1;
+            if(v1>v2)return 1;
+            else if(v2>v1) return -1;
              while(i<n || j<m){
-             a=helper(i,n,s1);
-             b=helper(j,m,s2);
-             if(a>b)return 1;
-             else if(b>a) return -1;
-        }
+             v1=helper(i,n,s1);
+             v2=helper(j,m,s2);
+             if(v1>v2)return 1;
+            else if(v2>v1) return -1;
+            }
         return 0;
         
     }
